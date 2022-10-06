af0f252752b5d22e423d6b030694f52aba3cb9c2
ihaze <ihaze@me.com>
2022-10-06 22:09:47 +0100

    Initial Commit

---
+++ PizzaBuzz/PizzaBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaBuzzApp.swift
+//  PizzaBuzz
+//
+//  Created by HaZe on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
